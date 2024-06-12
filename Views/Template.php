5318ddc8996667c893bc959cfbb145754e56681e
Mine25-afk <122749969+Mine25-afk@users.noreply.github.com>
2024-06-12 16:43:27 -0600

    afa

---
+++ Views/Template.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="Views/Resources/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="Views/Resources/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- HEADER -->
+  <?php include "Modules/Header.php"?>
+  <!-- /.HEADER -->
+
+  <!-- Main Sidebar Container -->
+  <?php include "Modules/Menu.php"?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+  
+
+ 
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <?php include "Modules/Footer.php"?>
+  
+
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="Views/Resources/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="Views/Resources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="Views/Resources/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="Views/Resources/dist/js/demo.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-$titutlo = 'Hola'
+    include "Controllers/Template.Controller.php";
+
+    $template = new ControllerTemplate;
+
+    $template -> ControllerTemplate();
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php echo $titutlo; ?> </title>
-</head>
-<body>
-    xd
-</body>
-</html>
